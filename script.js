eae764f12ab99f064ab18551a94367131aed4a51
Egitva <9026479948e@gmail.com>
2025-02-19 15:06:29 +0500

    efesw

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 var counter = 0;
+var mnoj = 1;
 var clicks = document.getElementById('clicks');
 var small_cookie_container = document.getElementById('small_cookie_container');
+var danila1 = document.getElementById('danila1');
 
 cookie_btn.addEventListener('click', () => {
-
     counter ++;
+    clicks.innerText = 'CLICKS: ' + counter * mnoj;
+});
+
+danila1.addEventListener = ('click', () => {
+    counter -= 50;
     clicks.innerText = 'CLICKS: ' + counter;
+    mnoj += 0.5;
 });

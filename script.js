7ceddb72dd5bc107bb1550414204d5714f969adf
Egitva <9026479948e@gmail.com>
2025-02-20 14:46:00 +0500

    yjrtth

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 var counter = 0;
 var mnoj = 1;
 var clicks = document.getElementById('clicks');
+var mnog = document.getElementById('mnog');
 var small_cookie_container = document.getElementById('small_cookie_container');
-var danila1 = document.getElementById('danila1');
+var danila = document.getElementById('danila');
 
 cookie_btn.addEventListener('click', () => {
-    counter ++;
-    clicks.innerText = 'CLICKS: ' + counter * mnoj;
+    counter += 1 * mnoj;
+    clicks.innerText = 'CLICKS: ' + counter;
 });
 
-danila1.addEventListener = ('click', () => {
-    counter -= 50;
-    clicks.innerText = 'CLICKS: ' + counter;
-    mnoj += 0.5;
-});
+danila.onclick = function() {
+    if (counter >= 50) {
+        counter -= 50
+        clicks.innerText = 'CLICKS: ' + counter;
+        mnoj += 0.5;
+        mnog.innerText = 'X' + mnoj;
+    }
+}
+
+danila2.onclick = function() {
+    if (counter >= 5000) {
+        counter -= 5000
+        clicks.innerText = 'CLICKS: ' + counter;
+        mnoj += 65;
+        mnog.innerText = 'X' + mnoj;
+    }
+}
